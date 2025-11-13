333491330a785570ae0969e1dca04a752fe8748a
Gabs4256 <gabrielvenancio85@gmail.com>
2025-11-13 09:37:18 -0300

    feat: add barbershop item component

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,24 @@ import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
 import banner from "../public/banner.png";
 import BookingItem from "./_components/booking-item";
+import { prisma } from "@/lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-const Home = () => {
+const Home = async () => {
+  const recomendedBarbershops = await prisma.barbershop.findMany({
+    orderBy:{
+      name:"asc",
+    },
+  });
+  const popularBarbershops = await prisma.barbershop.findMany({
+    orderBy:{
+      name:"desc",
+    },
+  });
   return (
     <main>
       <Header />
-      <div className="space-y-4 px-5">
+      <div className="space-y-4 p-5">
       <SearchInput />
       <Image 
         src={banner}
@@ -16,7 +28,7 @@ const Home = () => {
         sizes="100vw" 
         className="h-auto w-full"
       />
-      <h2 className="text-xs text-foreground font-semibold uppercase">
+      <h2 className="text-foreground text-xs font-semibold uppercase">
         Agendamentos
       </h2>
       <BookingItem
@@ -25,6 +37,23 @@ const Home = () => {
         barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
         date={new Date()}
       />
+      <h2 className="text-foreground text-xs font-semibold uppercase">
+        Recomendados
+      </h2> 
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+        {recomendedBarbershops.map((barbershop) => (
+        <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+      ))}
+        </div>
+
+      <h2 className="text-foreground text-xs font-semibold uppercase">
+        Recomendados
+      </h2> 
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+        {popularBarbershops.map((barbershop) => (
+        <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+      ))}
+        </div>
       </div>
     </main>
   );
