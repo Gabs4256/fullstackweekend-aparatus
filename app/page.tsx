9c63a44d184de281f86b308b2b486dc697936538
Gabs4256 <gabrielvenancio85@gmail.com>
2026-01-06 10:29:17 -0300

    feat: add bookings page

--- app/page.tsx
+++ app/page.tsx
@@ -6,65 +6,67 @@ import BookingItem from "./_components/booking-item";
 import { prisma } from "@/lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 import Footer from "./_components/footer";
-import { PageContainer, PageSection, PageSectionTitle, PageSectionScroller } from "./_components/ui/page";
+import {
+PageContainer,
+PageSection,
+PageSectionScroller,
+PageSectionTitle,
+} from "./_components/ui/page";
 
 const Home = async () => {
-  const recomendedBarbershops = await prisma.barbershop.findMany({
-    orderBy:{
-      name:"asc",
-    },
-  });
-  const popularBarbershops = await prisma.barbershop.findMany({
-    orderBy:{
-      name:"desc",
-    },
-  });
-  return (
-    <main>
-      <Header />
-      <PageContainer>
-      <SearchInput />
-      <Image 
-        src={banner}
-        alt="Agende agora!" 
-        sizes="100vw" 
-        className="h-auto w-full"
-      />
-      <PageSection>
-      <PageSectionTitle>
-        Agendamentos
-      </PageSectionTitle>
-      <BookingItem
-        serviceName="Corte de cabelo"
-        barbershopName="Barbearia do João"
-        barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
-        date={new Date()}
-      />
-      </PageSection>
-      <PageSection>
-      <PageSectionTitle>
-        Recomendados
-      </PageSectionTitle> 
-        <PageSectionScroller>
-        {recomendedBarbershops.map((barbershop) => (
-        <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-      ))}
-        </PageSectionScroller>
-      </PageSection>
-      <PageSection>
-      <PageSectionTitle>
-        Populares
-      </PageSectionTitle> 
-        <PageSectionScroller>
-        {popularBarbershops.map((barbershop) => (
-        <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-      ))}
-        </PageSectionScroller>
-      </PageSection>
-      </PageContainer>
-      <Footer/>
-    </main>
-  );
+const recommendedBarbershops = await prisma.barbershop.findMany({
+orderBy: {
+name: "asc",
+},
+});
+const popularBarbershops = await prisma.barbershop.findMany({
+orderBy: {
+name: "desc",
+},
+});
+return (
+<main>
+<Header />
+<PageContainer>
+<SearchInput />
+<Image
+src={banner}
+alt="Agende agora!"
+sizes="100vw"
+className="h-auto w-full"
+/>
+<PageSection>
+<PageSectionTitle>Agendamentos</PageSectionTitle>
+<BookingItem
+serviceName="Corte de cabelo"
+barbershopName="Barbearia do João"
+barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+date={new Date()}
+            status="confirmed"
+/>
+</PageSection>
+
+<PageSection>
+<PageSectionTitle>Recomendados</PageSectionTitle>
+<PageSectionScroller>
+{recommendedBarbershops.map((barbershop) => (
+<BarbershopItem key={barbershop.id} barbershop={barbershop} />
+))}
+</PageSectionScroller>
+</PageSection>
+
+<PageSection>
+<PageSectionTitle>Populares</PageSectionTitle>
+<PageSectionScroller>
+{popularBarbershops.map((barbershop) => (
+<BarbershopItem key={barbershop.id} barbershop={barbershop} />
+))}
+</PageSectionScroller>
+</PageSection>
+</PageContainer>
+<Footer />
+</main>
+);
 };
 
 export default Home;
